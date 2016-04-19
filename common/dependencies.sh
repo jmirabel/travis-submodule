80ccc060a66967cc756b3dedd345a6ef5e111cec
Joseph Mirabel <jmirabel@laas.fr>
2016-04-19 13:53:17 +0200

    Add linux and osx files

---
+++ common/dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+install_eigen3()
+{
+  cmake_args=${CMAKE_ARGS}
+  CMAKE_ARGS="${CMAKE_ARGS} -Dpkg_config_libdir='${DEVEL_DIR}/install/lib'"
+  install_tar_dependency "http://bitbucket.org/eigen/eigen/get/3.2.4.tar.bz2" "eigen-eigen-10219c95fe65" j
+  CMAKE_ARGS=${cmake_args}
+}
+
+install_doxygen()
+{
+    _cd_src_directory
+    echo "--> Installing Doxygen"
+    wget --quiet -O - "ftp://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.11.linux.bin.tar.gz" | tar -xz;
+    cd "$build_dir/doxygen-1.8.11"
+    ./configure --prefix "$DEVEL_DIR/install"
+    {
+      make $MAKE_ARGS install > ${MAKE_OUTPUTFILE}
+    } || {
+      :
+    }
+}
---
+++ common/functions.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Variable that can be configured
+: ${CMAKE_BUILD_TYPE=Release}
+: ${CMAKE_BUILD_REL_DIRECTORY=build}
+: ${CMAKE_ARGS="-DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX='${DEVEL_DIR}/install' -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"}
+: ${MAKE_ARGS="-s -j4"}
+: ${MAKE_OUTPUTFILE="${ROOT_DIR}/make.log"}
+: ${GIT_QUIET="--quiet"}
+
+# git commands
+: ${git=/usr/bin/git}
+: ${git_submodule="${git} submodule ${GIT_QUIET} update --init"}
+: ${git_checkout="${git} checkout ${GIT_QUIET}"}
+: ${git_clone="${git} clone ${GIT_QUIET}"}
+
+# create build directory inside source folder and run cmake and make
+_do_install()
+{
+  echo "--> Compiling $2"
+  [ ! -d "${CMAKE_BUILD_REL_DIRECTORY}" ] && mkdir --parents ${CMAKE_BUILD_REL_DIRECTORY}
+  cd ${CMAKE_BUILD_REL_DIRECTORY}
+  cmake ${CMAKE_ARGS} ..
+  make ${MAKE_ARGS} install >> ${MAKE_OUTPUTFILE}
+}
+_cd_src_directory()
+{
+  [ ! -d "${DEVEL_DIR}/src" ] && mkdir --parents "${DEVEL_DIR}/src"
+  cd "${DEVEL_DIR}/src"
+}
+
+# Install from tar
+# 3 mandatory options: url, extracted directory name, tar options (most importantly, the compression type if any. x will be prepended)
+# For instance:
+# install_tar_dependency "https://github.com/roboptim/roboptim-core/releases/download/v3.1/roboptim-core-3.1.tar.bz2" "roboptim-core-3.1" "j"
+install_tar_dependency()
+{
+  url=$1
+  directory=$2
+  _cd_src_directory
+  wget --quiet -O - "$url" | tar x$3;
+  cd "$directory"
+  _do_install "$directory"
+}
+
+
+# Git dependency
+install_git_dependency()
+{
+  url=$1
+  directory=$2
+  branch=$3
+  _cd_src_directory
+  $git_clone "$url" "$directory"
+  cd "$directory"
+  ${git_checkout} $branch
+  ${git_submodule}
+  _do_install "$directory"
+}
+
+install_github_dependency()
+{
+  url=git@github.com/$1
+  branch=$2
+  install_git_dependency $url $1 $branch
+}
